623842c264c9f76757c8805eec38faa5165b9cb5
Sang Oh <baul3oh@gmail.com>
2022-09-01 18:34:23 -0500

    completed set of arrays and it generates random password for now

--- script.js
+++ script.js
@@ -1,30 +1,34 @@
 // Assignment Code
 let generateBtn = document.querySelector("#generate");
 
-
+let lowAlpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+let upAlpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'].join("").toUpperCase();
+let numbset = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+let speChar = ['!', '@', '#', '$', '%', '^', '&', '*']
+let rndLetter = [...lowAlpha, ...upAlpha, ...numbset, ...speChar]
 
 // Write password to the #password input
 function writePassword() {
-  let password = generatePassword();
+  let pswd = genPassword();
   let passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  passwordText.value = pswd;
 
 }
 
 
-
-
 function genPassword() {
+
   let pswd = "";
-
   for (let i = 0; i < 25; i++) {
-    str += ""
+    
 
-    return pswd;
+    let rndNumb = Math.floor(Math.random()*rndLetter.length);
+    let rndChar = rndLetter[rndNumb]
+    pswd += rndChar;
     
   }
-
+  return pswd;
 }
 
 
