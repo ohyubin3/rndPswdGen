60a7c5bde9a4cfdf47e5b4ea3bf6cc324e3cb8e9
Sang Oh <baul3oh@gmail.com>
2022-09-01 18:01:19 -0500

    first save

---
+++ script.js
@@ -0,0 +1,32 @@
+// Assignment Code
+let generateBtn = document.querySelector("#generate");
+
+
+
+// Write password to the #password input
+function writePassword() {
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+
+
+
+function genPassword() {
+  let pswd = "";
+
+  for (let i = 0; i < 25; i++) {
+    str += ""
+
+    return pswd;
+    
+  }
+
+}
+
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
